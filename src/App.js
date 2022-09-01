60b12266d8991eb63dc82669bb2ba238f0350b1e
Aruzhan-Bekmakhanbetkyzy <aruzhanbekmakhanbetkyzy@gmail.com>
2022-09-02 05:28:56 +0900

    just commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import './index.scss';
+import React from "react";
+import "./index.scss";
+
+const Modal = ({ open, setOpen, children }) => (
+    <div className={`overlay animated ${open ? "show" : ""}`}>
+        <div className="modal">
+            <svg
+                onClick={() => setOpen(false)}
+                height="50"
+                viewBox="0 0 300 300"
+                width="50"
+            >
+                <title />
+                <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+            </svg>
+            {children}
+        </div>
+    </div>
+);
 
 function App() {
-  return (
-    <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
-      </div>
-    </div>
-  );
+    const [open, setOpen] = React.useState(false);
+
+    return (
+        <div className="App">
+            <button onClick={() => setOpen(true)} className="open-modal-btn">
+                ✨ Open Window
+            </button>
+            <Modal open={open} setOpen={setOpen}>
+                <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+                <h3>Bran explosion</h3>
+            </Modal>
+        </div>
+    );
 }
 
 export default App;
